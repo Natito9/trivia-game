10db88cdedc4e595c6e52d9c43e7a05613e21a5e
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-01-01 19:18:02 +0100

    mind map for js

---
+++ script.js
@@ -0,0 +1,26 @@
+//visual aspects
+
+//game start display
+//trivia display in main
+//correct and incorrent display
+//game ends display
+
+//logic
+
+//start game
+//select category (not priority?)
+// get question
+//select answer
+// check if answer is correct or not
+//update score 
+//display correct or incorrect pop up
+//click on next question or exit game
+//show next question
+// update the current question counter 2/10
+//end game
+//show final score and option to play again
+
+//need
+
+//api call
+//handle 404 error with pop up
